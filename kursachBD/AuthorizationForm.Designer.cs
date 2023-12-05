d6d60ba363d7375f2043a91207d438ed1f3045dc
SuperJK55 <stepan.kolenkin@mail.ru>
2023-12-06 01:15:49 +0600

    Added new form for authorization users

---
+++ kursachBD/AuthorizationForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace kursachBD
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.passwordRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.signInButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginRichTextBox
+            // 
+            this.loginRichTextBox.Location = new System.Drawing.Point(140, 119);
+            this.loginRichTextBox.Name = "loginRichTextBox";
+            this.loginRichTextBox.Size = new System.Drawing.Size(206, 33);
+            this.loginRichTextBox.TabIndex = 0;
+            this.loginRichTextBox.Text = "";
+            // 
+            // passwordRichTextBox
+            // 
+            this.passwordRichTextBox.Location = new System.Drawing.Point(140, 186);
+            this.passwordRichTextBox.Name = "passwordRichTextBox";
+            this.passwordRichTextBox.Size = new System.Drawing.Size(206, 33);
+            this.passwordRichTextBox.TabIndex = 1;
+            this.passwordRichTextBox.Text = "";
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginLabel.Location = new System.Drawing.Point(38, 121);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(96, 31);
+            this.loginLabel.TabIndex = 2;
+            this.loginLabel.Text = "Логин:";
+            this.loginLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordLabel.Location = new System.Drawing.Point(18, 188);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(116, 31);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Пароль:";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.passwordLabel.UseMnemonic = false;
+            // 
+            // signInButton
+            // 
+            this.signInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signInButton.Location = new System.Drawing.Point(170, 264);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(150, 40);
+            this.signInButton.TabIndex = 4;
+            this.signInButton.Text = "Войти";
+            this.signInButton.UseVisualStyleBackColor = true;
+            // 
+            // AuthorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.signInButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.passwordRichTextBox);
+            this.Controls.Add(this.loginRichTextBox);
+            this.Name = "AuthorizationForm";
+            this.Text = "AuthorizationForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox loginRichTextBox;
+        private System.Windows.Forms.RichTextBox passwordRichTextBox;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button signInButton;
+    }
+}
